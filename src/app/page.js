1ff0ea014234509877fc0e4aea6ed965523a4aaf
Ziad Ali <ziaddalii11@gmail.com>
2024-11-10 21:00:04 +0200

    update: cover-share image metadata

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,14 @@ import ExperienceSection from "@/components/sections/ExperienceSection";
 import HeroSection from "@/components/sections/HeroSection";
 import LetsConnectSection from "@/components/sections/LetsConnectSection";
 import WorksSection from "@/components/sections/WorksSection";
+import CoverShare from '@/app/cover-share.png';
 export const metadata = {
   title: "Ziad Ali",
   description:
     "Experienced frontend developer specializing in creating dynamic, user-centered web experiences. Passionate about React, digital solutions, and elevating customer engagement through optimized UI and seamless performance.",
   openGraph: {
     title: "Ziad Ali",
-    images: "../assets/images/cover-share.png",
+    images: CoverShare.src,
     description:
       "Experienced frontend developer specializing in creating dynamic, user-centered web experiences. Passionate about React, digital solutions, and elevating customer engagement through optimized UI and seamless performance.",
   },
