e0ec851cc2bd32a3c7769ac81be6836ecf2e491c
Ziad Ali <ziaddalii11@gmail.com>
2024-11-16 08:54:47 +0200

    Update: Hero Date to dayjs

--- src/components/Hero/Time.jsx
+++ src/components/Hero/Time.jsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import Star from "@/assets/images/star.png";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 function CurrentDate() {
-  const currentDate = new Date();
-  const day = String(currentDate.getDate()).padStart(2, "0");
-  const month = currentDate.toLocaleString("default", { month: "short" });
-  const year = currentDate.getFullYear();
+  const cairoTime = dayjs().tz("Africa/Cairo");
+  const day = cairoTime.format("DD");
+  const month = cairoTime.format("MMM");
+  const year = cairoTime.format("YYYY");
 
   return (
     <div className="current-date flex gap-2 text-secondary relative leading-none">
