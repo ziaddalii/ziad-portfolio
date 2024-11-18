9c20759223d04ad72c058af7382b45e4a31fdd1d
Ziad Ali <ziaddalii11@gmail.com>
2024-11-18 05:56:08 +0200

    Update: date component and remove dayjs

--- src/components/Hero/Time.jsx
+++ src/components/Hero/Time.jsx
@@ -1,24 +1,40 @@
+"use client";
 import Image from "next/image";
 import Star from "@/assets/images/star.png";
-import dayjs from "dayjs";
-import timezone from "dayjs/plugin/timezone";
-import utc from "dayjs/plugin/utc";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
+import { useState, useEffect } from "react";
 
 function CurrentDate() {
-  const cairoTime = dayjs().tz("Africa/Cairo");
-  const day = cairoTime.format("DD");
-  const month = cairoTime.format("MMM");
-  const year = cairoTime.format("YYYY");
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+
+  useEffect(() => {
+    const now = new Date();
+
+    const options = { timeZone: "Africa/Cairo" };
+    const cairoDate = new Intl.DateTimeFormat("en-US", options).format(now);
+
+    const cairoTime = new Date(cairoDate);
+    setDay(cairoTime.getDate().toString().padStart(2, "0"));
+    setMonth(cairoTime.toLocaleString("default", { month: "short" }));
+    setYear(cairoTime.getFullYear());
+  }, []);
 
   return (
     <div className="current-date flex gap-2 text-secondary relative leading-none">
-      <Image src={Star} width={40} height={40} alt="star" className="w-10 h-10 absolute -left-10 top-4" />
-      <span className="md:text-[10rem] sm:text-[9rem] text-[8rem] font-asset">{day}</span>
+      <Image
+        src={Star}
+        width={40}
+        height={40}
+        alt="star"
+        className="w-10 h-10 absolute -left-10 top-4"
+      />
+      <span className="md:text-[10rem] sm:text-[9rem] text-[8rem] font-asset">
+        {day}
+      </span>
       <div className="flex-col flex justify-center text-3xl font-playfair lowercase">
-        <span className="font-bold">{month}</span> <span className="text-xl">{year}</span>
+        <span className="font-bold">{month}</span>{" "}
+        <span className="text-xl">{year}</span>
       </div>
     </div>
   );
